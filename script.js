b50d24e5acf832737506ec0073cbb7e7881cdd51
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2024-01-21 21:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	date1 = new Date(date1);
+	date2 = new Date(date2);
+	let timeDiff = date2.getTime() - date1.getTime();
+
+	let daysDiff = timeDiff / (24 * 60 * 60 * 1000);
+
+	return daysDiff;
 };
 
 // Do not change the code below.
